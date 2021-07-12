3c65908e27975307d7339e8269c4f95ab3e734a4
Aakash Kumar <akashkmr27089@gmail.com>
2021-07-12 23:50:31 +0530

    Removing HyperLink

--- src/App.js
+++ src/App.js
@@ -5,20 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="row">
+          <div className="col-10">
+            Hi There this is Just A test
+          </div>
+          <div className="col-2">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        </div>
       </header>
-    </div>
+    </div >
   );
 }
 
